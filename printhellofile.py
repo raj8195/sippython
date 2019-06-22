559ed6f9b81ce4fa6b7440d89c5c355534692ae6
raj8195 <51732357+raj8195@users.noreply.github.com>
2019-06-22 17:44:32 +0530

    22nd June

---
+++ printhellofile.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(shashiraj)sip
+"""
+
+def printhello():
+    print("Hello")
+
+printhello()
+
+def printHello2(name):
+    print("Hii \t" +name)
+    
+printHello2()
+printHello2('Saksham')   
+    
+def printHello3(name='Student')
+    print("Hii \t" +name")
